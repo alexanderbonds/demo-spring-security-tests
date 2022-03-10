7d4072b5c5dd3600b8d17b27f00d6aaf5e6f0622
Alexander Bondarenko <friend.of.pans@gmail.com>
2022-03-10 11:46:58 +0300

    import fix

--- src/main/java/ru/abondarenko/dev/spring/controllers/SecuredController.java
+++ src/main/java/ru/abondarenko/dev/spring/controllers/SecuredController.java
@@ -1,8 +1,6 @@
 package ru.abondarenko.dev.spring.controllers;
 
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
